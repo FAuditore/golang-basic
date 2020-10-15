78b365cb51edc6b2957b3f8254d4dc2d37a11a94
FAuditore <951165464@qq.com>
2020-10-15 16:20:38 +0800

    &

--- interface.go
+++ interface.go
@@ -21,15 +21,43 @@ func main() {
 		var _ interface = name 	确保name变量实现了interface接口
 		var _ eat = animal
 
-		type iface struct{
-			tab *itab
+		type iface struct {
+			tab  *itab
 			data unsafe.Pointer
 		}
 
-		type eface struct{
-			_type *type
-			data unsafe.Pointer
+		type itab struct {
+			inter *interfacetype
+			_type *_type
+			hash  uint32 // copy of _type.hash. Used for type switches.
+			_     [4]byte
+			fun   [1]uintptr // variable sized. fun[0]==0 means _type does not implement inter.
 		}
+
+
+		type eface struct {
+			_type *_type
+			data  unsafe.Pointer
+		}
+
+		type _type struct {
+		size       uintptr
+		ptrdata    uintptr // size of memory prefix holding all pointers
+		hash       uint32
+		tflag      tflag
+		align      uint8
+		fieldAlign uint8
+		kind       uint8
+		// function for comparing objects of this type
+		// (ptr to object A, ptr to object B) -> ==?
+		equal func(unsafe.Pointer, unsafe.Pointer) bool
+		// gcdata stores the GC type data for the garbage collector.
+		// If the KindGCProg bit is set in kind, gcdata is a GC program.
+		// Otherwise it is a ptrmask bitmap. See mbitmap.go for details.
+		gcdata    *byte
+		str       nameOff
+		ptrToThis typeOff
+	}
 	*/
 	var _ USB = mouse{}
 
